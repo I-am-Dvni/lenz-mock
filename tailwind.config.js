a1d1bf83b7b0f95afa37cc116e857355a2eb5a27
Dani <daniali.ali.r@gmail.com>
2023-02-06 13:18:40 +0330

    chore: changed screen break points on tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "576px",
+      sm: "768px",
+      md: "992px",
+      lg: "1200px",
+      xl: "1400px",
+    },
     extend: {},
   },
   plugins: [],
