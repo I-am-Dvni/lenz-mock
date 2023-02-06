8013972bc5e35ac52a2237ba4bdb3e240c3809f0
Dani <daniali.ali.r@gmail.com>
2023-02-06 13:17:55 +0330

    chore: created login component, and it's path and set background color

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,6 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header class="mt-6"></header>
-
   <RouterView />
 </template>
 
---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="body"></div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
+
+<style scoped>
+.body {
+  background-color: var(--main-bg-color);
+  position: fixed;
+  padding: 0;
+  margin: 0;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
